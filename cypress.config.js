7fbd1d6e70ef0b47d8225127fd5a7c423ed4d148
Jetty Charan Tej <charantejj@gmail.com>
2026-05-13 21:49:37 +0530

    AI enabled

--- cypress.config.js
+++ cypress.config.js
@@ -13,16 +13,19 @@ async function setupNodeEvents(on, config) {
     "file:preprocessor",
     createBundler({
       plugins: [createEsbuildPlugin(config)],
+      // Forces esbuild to append source maps directly into the test file
+      sourcemap: "inline",
     }),
   );
   return config;
 }
 
 module.exports = defineConfig({
+  projectId: "d83pya",
   allowCypressEnv: true, // <-- Must be exactly here at the root level
   e2e: {
     setupNodeEvents,
-    specPattern: "cypress/e2e/**/*.feature", //if this is not defined, default one will be: filename.cy.js
+    // specPattern: "cypress/e2e/**/*.feature", //if this is not defined, default one will be: filename.cy.js
     supportFile: false,
     baseUrl: "https://saucedemo.com",
   },
--- cypress/e2e/first_spec.cy.js
+++ cypress/e2e/first_spec.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("AI createion of test case", () => {
+    cy.prompt(
+      [
+        "visit https://www.saucedemo.com/",
+        "type {{username}} in the email field",
+        "type {{password}} in the password field",
+        "click the login button",
+        "verify url changed contains inventory",
+      ],
+      {
+        placeholders: { username: "standard_user", password: "secret_sauce" },
+      },
+    );
+  });
+});
