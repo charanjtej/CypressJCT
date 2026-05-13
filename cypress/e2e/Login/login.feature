a4e6bb08f2e1ba101baa0bb5d2a2b9577ef9ba7c
Jetty Charan Tej <charantejj@gmail.com>
2026-05-13 14:54:05 +0530

    Updated Feature File to support Data Driven testing

--- cypress/e2e/Login/login.feature
+++ cypress/e2e/Login/login.feature
@@ -2,5 +2,10 @@ Feature: Login
 
 Scenario: Login with Valid Credentials
 Given I am on login page of application
-When  provide username and password 
+When  provide "<user_details>" and "<password_details>" 
 Then Verify the user logged in sucessfully
+
+Examples:
+    | user_details | password_details |
+    |standard_user|secret_sauce|
+    |problem_user|secret_sauce|
--- cypress/e2e/Login/login.js
+++ cypress/e2e/Login/login.js
@@ -4,9 +4,17 @@ Given(`I am on login page of application`, () => {
   cy.visit("/");
 });
 
+//hard coded passing of values in script
 When(`provide username and password`, () => {
   cy.get("#user-name").type("standard_user");
   cy.get("#password").type("secret_sauce");
+  cy.get("#login-button").click();
+});
+
+//Passing the vaules from the feature file.
+When(`provide {string} and {string}`, (username, password) => {
+  cy.get("#user-name").type(username);
+  cy.get("#password").type(password);
   cy.get("#login-button").click();
 });
 
